0303c28ea9d822b2c984b7f43528e48541a406f7
Özlem Nur Duman <ozlemnduman@stu.aydin.edu.tr>
2025-08-07 23:20:55 +0300

    login is added

--- src/main/java/stajokulu/mlipmp/api/controllers/LoginSignupController.java
+++ src/main/java/stajokulu/mlipmp/api/controllers/LoginSignupController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +21,7 @@ public class LoginSignupController {
 
     private final UserService userService;
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<String> loginUser(@RequestBody LoginDto loginDto) {
         try{
             userService.getUserByEmail(loginDto);
